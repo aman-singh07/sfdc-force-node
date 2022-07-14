94c508114364597e32d63a2960b053119489f7c9
amansingh-bib <105208786+amansingh-bib@users.noreply.github.com>
2022-07-14 09:38:49 +0530

    Added comment

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var hbs = require('hbs');
 var app = express();
 
 app.set('view engine', 'hbs');
-app.enable('trust proxy');
+app.enable('trust proxy'); 
 
 function isSetup() {
   return (process.env.CONSUMER_KEY != null) && (process.env.CONSUMER_SECRET != null);
